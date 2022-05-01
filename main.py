f67b9ee5790f8fd5252827dda51ff9c383a0776c
Bushra Hossain <87435687+BushraOnika@users.noreply.github.com>
2022-05-02 01:03:11 +0600

    BHOOOOOO did't liked python at allllllll ewwwwww

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+"""def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+"""
+              # lets learn how to print in pythonnnnnnnnn
+print("Hiiiiiiii Do you know meeeee but how cause I'm secretive about myself!!!!!-_-")
+        #Lets play with some variables.....
+name='Bushra'
+year=2022
+float_val=2.5
+print('\n',type(name),'\n',type(year),'\n',type(float_val)) # ohooooo its work ;)
+           #Taking Input from the user
+bhoo=input("Enter what you loved most: ")
+print(bhoo)
+#ohhooo Bush has tried enough for todayyyyyyyyyyyyyy yeeeeeeee......... Tired Bush sleeeppppppppppp
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
