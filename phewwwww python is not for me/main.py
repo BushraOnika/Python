09e52afcfca9f7d6da0ebf84aecc9538eb22e5ce
Bushra Hossain <87435687+BushraOnika@users.noreply.github.com>
2022-05-02 21:47:09 +0600

    Add files via upload
    Lets not focus on python anymore -_-

---
+++ phewwwww python is not for me/main.py
@@ -0,0 +1,34 @@
+
+#Seee this is how array oppsi List works
+
+name=["Eid","salami", 2022,6,"May"]
+print(name.index(2022))
+name.append(2023) #Adds an element at last of the list
+name.insert(2,"Treat")#Adds an element at the specified position
+name.reverse() #Reverses the order of the list
+num=[12,50,2,68,25,42,17,32,30,6]
+num.reverse()
+print(num) #we don't need loops to print list its really sweet of python like mee ;)
+num.sort()#it will sort in smaller to bigger order
+print(num)
+num.sort(reverse=True|False) #it will sort in bigger value to smaller values
+print(num)
+if(4==5):
+    print("4 greater than 5")
+elif(5>=6):
+    print("5 is less than 6")
+else: # don't forgot this colon ':' ewwwwwww hate this thing
+    print("I'm Bushraaaa")
+for i in "banana":
+  print(i)
+
+listt=["Bushra","Hossain","Onika"]
+for i in listt:
+      print(i)
+#function define shuuuuuushhhhhh
+def Oniy(name): # EHHHHHH don't forgot ':' ehhhhhhhhh
+     return name
+def Booo():
+    pass #nothing in this function silly Onikaaaaa
+print(Oniy("Bush"))
+print(Booo())
